eb7c47d33f0d2091e95ee35609fbe3871011881a
mouh2020 <badimohammed2019@gmail.com>
2023-11-24 16:48:25 +0100

    fix some issues

--- main.py
+++ main.py
@@ -5,17 +5,15 @@ import pandas as pd
 
 translator = Translator()
 df = pd.read_excel("All-4000.xlsx")
-current_row = starting_row
-for idx,title in enumerate(df[column_title].to_list()[starting_row:ending_row]) :
+for i in range(starting_row,ending_row) :
     while True :
         try :
-            translation = translator.translate(title,src=src_language,dest=dest_language)
-            df[column_title][current_row] = translation.text
+            translation = translator.translate(df["Title"][i],src=src_language,dest=dest_language)
+            df[column_title][i] = translation.text
             df.to_excel("translated.xlsx",index=False)
             break
         except Exception as e : 
             import time
             print(f"error occured : {str(e)}")
             time.sleep(5)
-    current_row+=1
-    print(f"{current_row} -origin text : {title} - translated to : {str(translation.text) if translation else None}")
+    print(f"{i} -origin text : {str(translation.origin) if translation else None} - translated to : {str(translation.text) if translation else None}")
