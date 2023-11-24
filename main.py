44007aa41df6b52068597342422ce36f467e4f62
mouh2020 <badimohammed2019@gmail.com>
2023-11-24 16:25:11 +0100

    change row num

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import pandas as pd
 
 translator = Translator()
 df = pd.read_excel("All-4000.xlsx")
-
-for idx,title in enumerate(df[column_title].to_list()[starting_row:ending_row],start=0) :
+current_row = starting_row
+for title in enumerate(df[column_title].to_list()[starting_row:ending_row]) :
     while True :
         try :
             translation = translator.translate(title,src="en",dest="ar")
-            df[column_title][idx] = translation.text
+            df[column_title][current_row] = translation.text
             df.to_excel("translated.xlsx",index=False)
             
             break
@@ -18,4 +18,4 @@ for idx,title in enumerate(df[column_title].to_list()[starting_row:ending_row],s
             import time
             print(f"error occured : {str(e)}")
             time.sleep(5)
-    print(f"{idx} -origin text : {title} - translated to : {str(translation.text) if translation else None}")
+    print(f"{current_row} -origin text : {title} - translated to : {str(translation.text) if translation else None}")
